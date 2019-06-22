157a8f778dc762b7ee6657036f2851aa8c5eeae5
Nite <47315009+Niteblock@users.noreply.github.com>
2019-06-22 12:05:54 +0200

    Create checks.py

---
+++ utils/checks.py
@@ -0,0 +1,4 @@
+#check for is_admin() if they are a dev
+#check for role_in_support("rolename")
+#check for is_support_guild() if the command is in support
+#check for commands.check(bot_has_admin())if the bot has admin (used as a bot.check)
