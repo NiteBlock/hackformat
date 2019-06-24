79c45564d57db12a35e77ff5350aff4287d70848
cheekysim <minecraftcheekysim@gmail.com>
2019-06-24 19:12:14 +0100

    Started /server command

--- cogs/reactionaction.py
+++ cogs/reactionaction.py
@@ -1,3 +1,25 @@
-#complex thing like format but from reactions
-# using utils.codeaction soon:tm:
-#cheeky was here
+import discord
+from discord.ext import commands
+import asyncio
+
+
+class reactions(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def server(self, ctx):
+        msg = await ctx.send(":regional_indicator_s: Server Settings\n :regional_indicator_u: User Settings")
+        reactions = ["🇸", "🇺"]
+        for r in reactions:
+            await msg.add_reaction(r)
+        await asyncio.sleep(5 / 10)
+
+        def check(reaction, user):
+            return user == ctx.author and reaction == reaction
+
+        reaction, user = await self.bot.wait_for('raw_reaction_add', check=check)
+        print(reaction, user)
+
+def setup(bot):
+    bot.add_cog(reactions(bot))
