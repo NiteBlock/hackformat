dc619216e534c3039f0462e548a31210b8b28636
cheekysim <minecraftcheekysim@gmail.com>
2019-06-25 07:01:29 +0100

    testing purposes...

--- cogs/reactionaction.py
+++ cogs/reactionaction.py
@@ -1,25 +1,25 @@
+import discord
 from discord.ext import commands
 import asyncio
 
 
-class Reactions(commands.Cog):
+class reactions(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def server(self, ctx):
+    async def test(self, ctx):
         msg = await ctx.send(":regional_indicator_s: Server Settings\n :regional_indicator_u: User Settings")
         reactions = ["🇸", "🇺"]
         for r in reactions:
             await msg.add_reaction(r)
         await asyncio.sleep(5 / 10)
-
-        def check(reaction, user):
-            return user == ctx.author and reaction == reaction
-
-        reaction, user = await self.bot.wait_for('raw_reaction_add', check=check)
-        print(reaction, user)
-
+        reaction = await self.bot.wait_for('raw_reaction_add')
+        if reaction.user_id == ctx.author.id:
+            if str(reaction.emoji) == reactions[0]:
+                await ctx.send(reactions[0])
+            elif str(reaction.emoji) == reactions[1]:
+                await ctx.send(reactions[1])
 
 def setup(bot):
-    bot.add_cog(Reactions(bot))
+    bot.add_cog(reactions(bot))
